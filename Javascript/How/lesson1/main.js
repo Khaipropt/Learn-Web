437a0ddb83b8fa67e8cd6f8c5336bb07825eb1e3
Khaipropt <duongkhai222204pt@gmail.com>
2025-02-19 15:12:55 +0700

    Toan tu so hoc -jv

--- Javascript/How/lesson1/main.js
+++ Javascript/How/lesson1/main.js
@@ -1,33 +1,19 @@
-/**
- * Gioi thieu ve toan tu trong JavaScript
- * 1. Toan tu so hoc - Arithmectic
- * 2. Toan tu gan - Assignment
- * 3. Toan tu so sanh - Comparison
- * 4. Toan tu logiv - Logical
- 
-*/
+/** Toan tu so hoc
+ * + --> Cong
+ * - --> Tru
+ * * --> Nhan
+ * ** --> Luy thua
+ * / --> Chia
+ * % --> Chia lay phan du
+ * ++ --> Tang 1 gia tri
+ * -- --> Giam 1 gia tri
+ *
+ */
 
-// 1. Toan tu so hoc - Arithmectic
-// var a = 1 * 2;
-// console.log(a);
-
-// 2. Toan tu gan - Assignment
-var fullName = 'Duong Quang Khai';
-
-// 3. Toan tu so sang - Comparison
-
-// var a = 1;
-// var b = 2;
-
-// if( a < b ) {
-//     alert("Dung");
-// }
-
-// 4. Toan tu logic - Logical
-
-var a = 1;
+var a = 4;
 var b = 2;
 
-if( a > 0 && b > 0 ) {
-    alert('a va b lon hon 0');
-}
+var c = a++;
+c++;
+ 
+console.log(c); 
